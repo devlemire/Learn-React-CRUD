202c758d59c7d5d9cd54c73d6634c2ce4caa3275
James Lemire <webdev.jameslemire@gmail.com>
2016-09-01 16:04:36 -0600

    Setting up your react app

---
+++ public/app/reactApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var App = React.createClass({
+  render() {
+    return (
+      <div>
+        <p>Hello World</p>
+      </div>
+    )
+  }
+})
