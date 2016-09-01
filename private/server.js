f659d50e30182daffe5faed99e5ff9b9c7c916d4
James Lemire <webdev.jameslemire@gmail.com>
2016-09-01 16:46:32 -0600

    Setup endpoints

--- private/server.js
+++ private/server.js
@@ -10,8 +10,28 @@ app.set('db', massiveInstance);
 app.use(cors({origin: 'http://localhost:3000/'}));
 app.use(bodyParser.json());
 
-app.get('/api/data', function() {
+app.post('/api/add/person', function(req, res) {
+  db.addPerson([req.body.first_name, req.body.last_name], function(err, r) {
+    res.status(200).send('Person created');
+  });
+})
 
+app.get('/api/people', function(req, res) {
+  db.getPeople(function(err, r) {
+    res.json(r);
+  });
 });
+
+app.post('/api/update/person', function(req, res) {
+  db.updatePerson([req.body.first_name, req.body.last_name], function(err, r) {
+    res.status(200).send('Person updated');
+  })
+})
+
+app.delete('/api/person/:personId', function(req, res) {
+  db.deletePerson([req.params.personId], function(err, r) {
+    res.status(200).send('Person deleted');
+  })
+})
 
 app.listen(3000, function() { console.log('Server started on port 3000'); });
