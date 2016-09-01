24e1611ee8bef9a8376eca0615d97749bce8db6a
James Lemire <webdev.jameslemire@gmail.com>
2016-09-01 17:04:57 -0600

    updates

--- public/app/reactApp.js
+++ public/app/reactApp.js
@@ -5,17 +5,36 @@ import axios from 'axios';
 var App = React.createClass({
   getInitialState() {
     return {
+      first_name: '',
+      last_name: '',
       people: []
     }
   }
   render() {
     return (
       <div>
-        <input type="text" />
-        <input type="text" />
-        <button>Add Person</button>
+        <h1>Add Person</h1>
+        <input onChange={this.first_nameCatcher} type="text" />
+        <input onChange={this.last_nameCatcher} type="text" />
+        <button onClick={this.addPerson}>Add Person</button>
       </div>
     )
+  },
+  first_nameCatcher(event) {
+    this.setState() {
+      first_name: event.target.value
+    }
+  },
+  last_nameCatcher(event) {
+    this.setState() {
+      last_name: event.target.value
+    }
+  },
+  addPerson() {
+    axios({
+      method: 'POST',
+      url: 'http://localhost:3000/api/add/person'
+    })
   }
 })
 
